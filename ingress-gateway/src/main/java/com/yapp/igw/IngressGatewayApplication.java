b6c2d8a63a2562a0d42bed4c3b07fd6b6395167f
Sehwan Lim <dla612@naver.com>
2025-06-19 23:50:53 +0900

    [Y26W2-26] build: ingress-gateway 모듈 생성
    Signed-off-by: Sehwan Lim <dla612@naver.com>

---
+++ ingress-gateway/src/main/java/com/yapp/igw/IngressGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yapp.igw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngressGatewayApplication.class, args);
+	}
+
+}
---
+++ ingress-gateway/src/test/java/com/yapp/igw/IngressGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.igw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
