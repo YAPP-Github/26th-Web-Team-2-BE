01459d31137d9ccef996f5155fe7046fb8e476ab
Sungyeon <csy357@gmail.com>
2025-06-25 13:56:22 +0900

    [Y26W2-42] feat: health check controller

---
+++ src/main/java/com/yapp/backend/swagger/HealthCheckDocs.java
@@ -0,0 +1,14 @@
+package com.yapp.backend.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "HEALTH CHECK API", description = "헬스 체크 테스트 API")
+public interface HealthCheckDocs {
+
+    @Operation(summary = "연결 테스트", description = "...")
+    ResponseEntity<String> healthCheck();
+
+
+}
