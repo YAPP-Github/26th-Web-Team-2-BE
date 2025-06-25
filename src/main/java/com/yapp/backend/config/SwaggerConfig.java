837b8c8d9c735ea7f3a0b30b17e94be3d03028b3
Sungyeon <csy357@gmail.com>
2025-06-25 13:54:33 +0900

    [Y26W2-42] build: swagger-openapi 의존성 및 설정 추가

---
+++ src/main/java/com/yapp/backend/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.yapp.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title       = "YAPP26 WEB2",
+                version     = "v1",
+                description = "HZZ 서비스 API 명세"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+
+}
