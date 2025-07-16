9aa90a32ffa078e2e716b5e0f249361d21ac04a3
Sungyeon <csy357@gmail.com>
2025-07-16 14:21:58 +0900

    [Y26W2-23] cicd: vm 배포에서 cloud run 배포로 변경

---
+++ .github/workflows/script/deploy-to-cloud-run.sh
@@ -0,0 +1,21 @@
+PROJECT_ID=$1
+REGION=$2
+REPO_NAME=$3
+IMAGE_NAME=$4
+IMAGE_SHA=$5
+
+SECRETS=$(gcloud secrets list --project=$PROJECT_ID --format="value(name)")
+
+UPDATE_SECRETS=""
+for SECRET in $SECRETS
+do
+    UPDATE_SECRETS+="--update-secrets ${SECRET}=projects/${PROJECT_ID}/secrets/${SECRET}/versions/latest "
+done
+
+gcloud run deploy ${IMAGE_NAME} \
+  --image ${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${IMAGE_NAME}:${IMAGE_SHA} \
+  --region ${REGION} \
+  --platform managed \
+  --memory 1Gi \
+  --cpu 2 \
+  $UPDATE_SECRETS
