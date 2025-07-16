bfd5fbe12fbe82f849c1cc77b1b3e84985c68b60
Sungyeon <csy357@gmail.com>
2025-07-16 23:14:24 +0900

    [Y26W2-23] fix: update secret 배열 형식으로 수정

--- script/deploy-to-cloud-run.sh
+++ script/deploy-to-cloud-run.sh
@@ -4,20 +4,18 @@ REPO_NAME=$3
 IMAGE_NAME=$4
 IMAGE_SHA=$5
 
-SECRETS=$(gcloud secrets list --project=$PROJECT_ID --format="value(name)")
+IFS=$'\n' read -rd '' -a SECRETS <<<"$(gcloud secrets list --project=$PROJECT_ID --format='value(name)')"
 
-UPDATE_SECRETS=""
-for SECRET in $SECRETS
-do
-    UPDATE_SECRETS+="--update-secrets ${SECRET}=projects/${PROJECT_ID}/secrets/${SECRET}/versions/latest "
+UPDATE_SECRETS=()
+for SECRET in "${SECRETS[@]}"; do
+    echo "Adding secret: $SECRET"
+    UPDATE_SECRETS+=(--update-secrets "${SECRET}=projects/${PROJECT_ID}/secrets/${SECRET}/versions/latest")
 done
 
-echo "UPDATE_SECRETS: $UPDATE_SECRETS"
-
-gcloud run deploy ${IMAGE_NAME} \
-  --image ${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${IMAGE_NAME}:${IMAGE_SHA} \
-  --region ${REGION} \
+gcloud run deploy "${IMAGE_NAME}" \
+  --image "${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${IMAGE_NAME}:${IMAGE_SHA}" \
+  --region "${REGION}" \
   --platform managed \
   --memory 1Gi \
   --cpu 2 \
-  $UPDATE_SECRETS
+  "${UPDATE_SECRETS[@]}"
