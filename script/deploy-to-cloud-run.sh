abdc42862268e2475e1beefbf202bc7de6930348
Sungyeon <csy357@gmail.com>
2025-07-17 00:55:26 +0900

    [Y26W2-23] test: gcloud debug log

--- script/deploy-to-cloud-run.sh
+++ script/deploy-to-cloud-run.sh
@@ -18,5 +18,5 @@ gcloud run deploy "${IMAGE_NAME}" \
   --platform managed \
   --memory 1Gi \
   --cpu 2 \
-  --update-secrets DB_PRIVATE_IP=projects/${PROJECT_ID}/secrets/DB_PRIVATE_IP/versions/latest
-#  "${UPDATE_SECRETS[@]}"
+  --verbosity debug \
+  "${UPDATE_SECRETS[@]}"
