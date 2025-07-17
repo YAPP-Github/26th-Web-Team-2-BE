4d416e39dd796cef70d13e21b4ee2fb03a88b13d
Sungyeon <csy357@gmail.com>
2025-07-18 01:11:38 +0900

    [Y26W2-119] feat: user domain, entity 추가

--- src/main/java/com/yapp/backend/repository/entity/UserEntity.java
+++ src/main/java/com/yapp/backend/repository/entity/UserEntity.java
@@ -1,15 +1,63 @@
 package com.yapp.backend.repository.entity;
 
+import com.yapp.backend.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+@Entity
 @Getter
-@Setter
+@Builder
+@Table(
+        name = "users",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = { "provider", "social_id" }
+        ))
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String provider;
+    private String socialId;
+    private String email;
+    private String nickname;
+    private String profileImage;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+
+    public static UserEntity from(User user) {
+        return UserEntity.builder()
+                .id(null)
+                .provider(user.getProvider())
+                .socialId(user.getSocialId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .profileImage(user.getProfileImage())
+                .createdAt(user.getCreatedAt())
+                .build();
+    }
+
+    public User toModel() {
+        return User.builder()
+                .id(id)
+                .provider(provider)
+                .socialId(socialId)
+                .email(email)
+                .nickname(nickname)
+                .profileImage(profileImage)
+                .build();
+    }
 }
