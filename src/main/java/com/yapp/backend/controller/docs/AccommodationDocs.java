81797a17273f163c0501b8f31765a6dc72f61834
SeHwan Lim <76430979+Torres-09@users.noreply.github.com>
2025-07-22 21:50:24 +0900

    [Y26W2-133] 숙소 카드 개수 조회 API 개발 (#23)
    * [Y26W2-133] feat: 숙소 카드 개수 조회 Mock API
    
    - 숙소 카드 개수 조회 Mock API
    
    * [Y26W2-133] feat: 매개 변수 require 속성 변경

--- src/main/java/com/yapp/backend/controller/docs/AccommodationDocs.java
+++ src/main/java/com/yapp/backend/controller/docs/AccommodationDocs.java
@@ -1,6 +1,7 @@
 package com.yapp.backend.controller.docs;
 
 import com.yapp.backend.common.response.StandardResponse;
+import com.yapp.backend.controller.dto.response.AccommodationCountResponse;
 import com.yapp.backend.controller.dto.response.AccommodationPageResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,6 +19,12 @@ public interface AccommodationDocs {
 		@Parameter(in = ParameterIn.QUERY, schema = @Schema(type = "integer"), description = "숙소가 포함된 테이블의 ID") Integer tableId,
 		@Parameter(in = ParameterIn.QUERY, schema = @Schema(type = "integer"), description = "페이지 번호") Integer page,
 		@Parameter(in = ParameterIn.QUERY, schema = @Schema(type = "integer"), description = "페이지 크기") Integer size,
+		@Parameter(in = ParameterIn.QUERY, schema = @Schema(type = "integer", format = "int64"), description = "유저 ID, 없는 경우 모든 유저가 생성한 숙소 목록을 반환합니다.") Long userId
+	);
+
+	@Operation(summary = "테이블 숙소 개수 조회", description = "테이블에 포함된 숙소의 개수를 조회합니다.")
+	ResponseEntity<StandardResponse<AccommodationCountResponse>> getAccommodationCountByTableId(
+		@Parameter(in = ParameterIn.QUERY, schema = @Schema(type = "integer"), description = "숙소가 포함된 테이블의 ID") Long tableId,
 		@Parameter(in = ParameterIn.QUERY, schema = @Schema(type = "integer", format = "int64"), description = "유저 ID, 없는 경우 모든 유저가 생성한 숙소 목록을 반환합니다.") Long userId
 	);
 }
---
+++ src/main/java/com/yapp/backend/controller/dto/response/AccommodationCountResponse.java
@@ -0,0 +1,14 @@
+package com.yapp.backend.controller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccommodationCountResponse {
+	private Long accommodationCount;
+}
