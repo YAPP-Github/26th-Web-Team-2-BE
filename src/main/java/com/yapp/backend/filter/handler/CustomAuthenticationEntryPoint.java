73c12ac755f290e5aadb783c643f9cdb04625bd5
Sungyeon <csy357@gmail.com>
2025-07-23 20:54:07 +0900

    [Y26W2-155] fix: entry point uri 추가

--- src/main/java/com/yapp/backend/filter/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/yapp/backend/filter/handler/CustomAuthenticationEntryPoint.java
@@ -15,13 +15,14 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
         String uri = request.getRequestURI();
-        // Swagger 요청이면 401이 아니라 그냥 허용 or Forbidden 응답
-        if (uri.startsWith("/swagger") || uri.startsWith("/v3/api-docs") || uri.startsWith("/swagger-ui")) {
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Swagger access forbidden");
+        if (uri.equals("/") ||
+                uri.startsWith("/swagger") ||
+                uri.startsWith("/v3/api-docs") ||
+                uri.startsWith("/swagger-ui")) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             return;
         }
 
-        // 그 외 요청에 대해서는 기존처럼 401 반환
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
