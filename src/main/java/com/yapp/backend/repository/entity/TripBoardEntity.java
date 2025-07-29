b3456536b2167f5e95513004523e1ce3b03ff84b
Sungyeon <csy357@gmail.com>
2025-07-30 00:02:39 +0900

    [Y26W2-156] refactor: tripGroup을 tripBoard로 네이밍 수정, id 네이밍 수정

--- src/main/java/com/yapp/backend/repository/entity/TripBoardEntity.java
+++ src/main/java/com/yapp/backend/repository/entity/TripBoardEntity.java
@@ -19,7 +19,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.web.service.annotation.DeleteExchange;
 
 @Getter
 @Entity
