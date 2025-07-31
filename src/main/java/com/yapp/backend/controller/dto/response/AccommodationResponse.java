377cece0528e7505286ce9bf1e91d30228e729a6
SeHwan Lim <76430979+Torres-09@users.noreply.github.com>
2025-07-31 23:37:35 +0900

    [Y26W2-208] 숙소 목록 조회 및 조회 API 응답에 예약사이트 로고 추가 (#43)
    * [Y26W2-208] feat: 사이트 별 logo URL 추가
    
    * [Y26W2-208] fix: boardId 누락 추가

--- src/main/java/com/yapp/backend/controller/dto/response/AccommodationResponse.java
+++ src/main/java/com/yapp/backend/controller/dto/response/AccommodationResponse.java
@@ -1,5 +1,6 @@
 package com.yapp.backend.controller.dto.response;
 
+import com.yapp.backend.common.util.SiteLogoUtil;
 import com.yapp.backend.service.model.Accommodation;
 import com.yapp.backend.service.model.Amenity;
 import com.yapp.backend.service.model.Attraction;
@@ -12,7 +13,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -23,6 +23,7 @@ public class AccommodationResponse {
 	private Long id;
 	private String url;
 	private String siteName;
+	private String logoUrl;
 	private String memo;
 	private LocalDateTime createdAt;
 	private LocalDateTime updatedAt;
@@ -48,31 +49,32 @@ public class AccommodationResponse {
 
 	public static AccommodationResponse from(Accommodation accommodation) {
 		return AccommodationResponse.builder()
-			.id(accommodation.getId())
-			.url(accommodation.getUrl())
-			.siteName(accommodation.getSiteName())
-			.memo(accommodation.getMemo())
-			.createdAt(accommodation.getCreatedAt())
-			.updatedAt(accommodation.getUpdatedAt())
-			.createdBy(accommodation.getCreatedBy())
-			.boardId(accommodation.getBoardId())
-			.accommodationName(accommodation.getAccommodationName())
-			.images(accommodation.getImages())
-			.address(accommodation.getAddress())
-			.latitude(accommodation.getLatitude())
-			.longitude(accommodation.getLongitude())
-			.lowestPrice(accommodation.getLowestPrice())
-			.highestPrice(accommodation.getHighestPrice())
-			.currency(accommodation.getCurrency())
-			.reviewScore(accommodation.getReviewScore())
-			.cleanlinessScore(accommodation.getCleanlinessScore())
-			.reviewSummary(accommodation.getReviewSummary())
-			.hotelId(accommodation.getHotelId())
-			.nearbyAttractions(accommodation.getNearbyAttractions())
-			.nearbyTransportation(accommodation.getNearbyTransportation())
-			.amenities(accommodation.getAmenities())
-			.checkInTime(accommodation.getCheckInTime())
-			.checkOutTime(accommodation.getCheckOutTime())
-			.build();
+				.id(accommodation.getId())
+				.url(accommodation.getUrl())
+				.siteName(accommodation.getSiteName())
+				.logoUrl(SiteLogoUtil.getLogoUrl(accommodation.getSiteName()))
+				.memo(accommodation.getMemo())
+				.createdAt(accommodation.getCreatedAt())
+				.updatedAt(accommodation.getUpdatedAt())
+				.createdBy(accommodation.getCreatedBy())
+				.boardId(accommodation.getBoardId())
+				.accommodationName(accommodation.getAccommodationName())
+				.images(accommodation.getImages())
+				.address(accommodation.getAddress())
+				.latitude(accommodation.getLatitude())
+				.longitude(accommodation.getLongitude())
+				.lowestPrice(accommodation.getLowestPrice())
+				.highestPrice(accommodation.getHighestPrice())
+				.currency(accommodation.getCurrency())
+				.reviewScore(accommodation.getReviewScore())
+				.cleanlinessScore(accommodation.getCleanlinessScore())
+				.reviewSummary(accommodation.getReviewSummary())
+				.hotelId(accommodation.getHotelId())
+				.nearbyAttractions(accommodation.getNearbyAttractions())
+				.nearbyTransportation(accommodation.getNearbyTransportation())
+				.amenities(accommodation.getAmenities())
+				.checkInTime(accommodation.getCheckInTime())
+				.checkOutTime(accommodation.getCheckOutTime())
+				.build();
 	}
 }
