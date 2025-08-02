0db32581d79522fa716f0ba7e539dee140baaef2
Sehwan Lim <dla612@naver.com>
2025-08-02 17:23:10 +0900

    [Y26W2-197] feat: Docs Valid 어노테이션 추가

--- src/main/java/com/yapp/backend/controller/docs/TripBoardDocs.java
+++ src/main/java/com/yapp/backend/controller/docs/TripBoardDocs.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,6 @@ public interface TripBoardDocs {
 
         @Operation(summary = "여행 보드 생성", description = "새로운 여행 보드를 생성합니다. JWT 인증을 통해 현재 사용자 정보를 추출하고, 생성자는 자동으로 OWNER 역할로 등록되며 고유한 초대 링크가 생성됩니다.")
         ResponseEntity<StandardResponse<TripBoardCreateResponse>> createTripBoard(
-                        @RequestBody TripBoardCreateRequest request,
+                        @RequestBody @Valid TripBoardCreateRequest request,
                         @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails);
 }
