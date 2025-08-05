89b37cb9205c49292aaf0919a98f45b9f05a5b00
Sungyeon <csy357@gmail.com>
2025-08-06 00:27:49 +0900

    [Y26W2-224] refactor: spring security oauth 의존성 및 로직 제거

--- src/main/java/com/yapp/backend/config/SecurityConfig.java
+++ src/main/java/com/yapp/backend/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.yapp.backend.config;
 
 import com.yapp.backend.filter.JwtFilter;
 import com.yapp.backend.filter.handler.CustomAuthenticationEntryPoint;
-import com.yapp.backend.filter.handler.OAuth2AuthenticationSuccessHandler;
-import com.yapp.backend.filter.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +14,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -29,8 +27,6 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-        private final CustomOAuth2UserService customOAuth2UserService;
-        private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
         private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
         private final JwtFilter jwtFilter;
 
@@ -63,36 +59,26 @@ public class SecurityConfig {
                                 .build();
         }
 
-        // todo UT 이후 재 반영 필요
         // 일반 API용 JWT 인증 설정
-//        @Bean
-//        @Order(2)
-//        public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
-//                return http
-//                                .csrf(AbstractHttpConfigurer::disable)
-//                                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // WebConfig의 CORS 설정
-//                                                                                                   // 사용
-//                                .formLogin(AbstractHttpConfigurer::disable)
-//                                .addFilterBefore(jwtFilter, OAuth2LoginAuthenticationFilter.class)
-//                                .authorizeHttpRequests(authorize -> authorize
-//                                                .requestMatchers(
-//                                                                "/")
-//                                                .permitAll()
-//                                                .requestMatchers(
-//                                                                "/oauth2/authorization/**",
-//                                                                "/api/**",
-//                                                                "/oauth/authorize", // OAuth2 Authorization Endpoint
-//                                                                "/login/oauth2/**" // OAuth2 code Redirect URI
-//                                                ).permitAll()
-//                                                .anyRequest().authenticated())
-//                                .oauth2Login(oauth2 -> oauth2
-//                                                .userInfoEndpoint(userInfo -> userInfo
-//                                                                .userService(customOAuth2UserService))
-//                                                .successHandler(oAuth2AuthenticationSuccessHandler))
-//                                .exceptionHandling(exception -> exception
-//                                                .authenticationEntryPoint(customAuthenticationEntryPoint))
-//                                .build();
-//        }
+        @Bean
+        @Order(2)
+        public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+                return http
+                                .csrf(AbstractHttpConfigurer::disable)
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // WebConfig의 CORS 설정
+                                .formLogin(AbstractHttpConfigurer::disable)
+                                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                                .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers(
+                                                                "/",
+                                                                "/api/**",
+                                                                "/oauth/authorize" // OAuth2 Authorization Endpoint
+                                                ).permitAll()
+                                                .anyRequest().authenticated())
+                                .exceptionHandling(exception -> exception
+                                                .authenticationEntryPoint(customAuthenticationEntryPoint))
+                                .build();
+        }
 
         @Bean
         public UserDetailsService userDetailsService() {
