0734873a6c25c84a222cf16afaa1f8b1a190fd4a
Sehwan Lim <dla612@naver.com>
2025-08-08 01:14:11 +0900

    [Y26W2-200] feat: DTO 및 예외 클래스, 코드 추가

---
+++ src/main/java/com/yapp/backend/controller/dto/response/TripBoardDeleteResponse.java
@@ -0,0 +1,16 @@
+package com.yapp.backend.controller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TripBoardDeleteResponse {
+    private Long tripBoardId;
+}
