8141a45b63363539411d11cab2eed88ca678a535
Sungyeon <csy357@gmail.com>
2025-08-09 03:44:58 +0900

    [Y26W2-211] feat: soft delete 로 deleteAt에 탈퇴날짜 저장

--- src/main/java/com/yapp/backend/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/yapp/backend/repository/impl/UserRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.yapp.backend.repository.impl;
 import static com.yapp.backend.common.exception.ErrorCode.*;
 
 import com.yapp.backend.common.exception.UserNotFoundException;
+import com.yapp.backend.repository.mapper.UserMapper;
 import com.yapp.backend.service.model.User;
 import com.yapp.backend.repository.JpaUserRepository;
 import com.yapp.backend.repository.entity.UserEntity;
@@ -14,10 +15,13 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
     private final JpaUserRepository jpaUserRepository;
+    private final UserMapper userMapper;
 
     @Override
     public User findByIdOrThrow(Long id) {
+        // TODO : 여행 보드 참여자 리스트를 조회할 때, 문제가 생길 여지가 있는지 확인
         return jpaUserRepository.findById(id)
+                .filter(u -> u.getDeletedAt() != null)
                 .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND))
                 .toModel();
     }
@@ -26,15 +30,21 @@ public class UserRepositoryImpl implements UserRepository {
     public User getUserBySocialUserInfoOrCreateUser(User socialUserInfo) {
         return jpaUserRepository
                 .findByProviderAndSocialId(socialUserInfo.getProvider(), socialUserInfo.getSocialId())
+                .filter(u -> u.getDeletedAt() != null)
                 .orElseGet(() ->
                         jpaUserRepository.save(UserEntity.from(socialUserInfo)))
                 .toModel();
     }
 
-    // TODO: 회원 탈퇴 기능 구현
+    // TODO: 회원 탈퇴 기능 구현 (hard delete)
     @Override
     public void deleteById(Long userId) {
 
+    }
+
+    @Override
+    public void saveUser(User findUser) {
+        jpaUserRepository.save(userMapper.domainToEntity(findUser));
     }
 
 }
--- src/main/java/com/yapp/backend/service/UserService.java
+++ src/main/java/com/yapp/backend/service/UserService.java
@@ -1,7 +1,15 @@
 package com.yapp.backend.service;
 
 import com.yapp.backend.service.model.User;
+import com.yapp.backend.controller.dto.response.WithdrawResponse;
 
 public interface UserService {
     User getUserById(Long id);
+    
+    /**
+     * 회원탈퇴 처리 (Soft Delete)
+     * @param userId
+     * @return
+     */
+    Boolean withdrawUser(Long userId);
 }
