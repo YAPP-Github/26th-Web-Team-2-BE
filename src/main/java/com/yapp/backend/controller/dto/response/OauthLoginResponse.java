baad325da84e8b7526bfb06bfa93dac935837e52
Sungyeon <csy357@gmail.com>
2025-08-09 02:04:01 +0900

    [Y26W2-210] refactor: record 를 class로 수정

--- src/main/java/com/yapp/backend/controller/dto/response/OauthLoginResponse.java
+++ src/main/java/com/yapp/backend/controller/dto/response/OauthLoginResponse.java
@@ -1,12 +1,13 @@
 package com.yapp.backend.controller.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 @Schema(description = "OAuth 로그인 응답")
-public record OauthLoginResponse(
-        @Schema(description = "사용자 ID")
-        Long userId,
-        
-        @Schema(description = "사용자 닉네임")
-        String nickname
-) {}
+public class OauthLoginResponse {
+        private Long userId;
+        private String nickname;
+}
