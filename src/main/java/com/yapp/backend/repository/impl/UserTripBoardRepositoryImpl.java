9400a88770f821e40769b8a3977b209d4f86c237
Sehwan Lim <dla612@naver.com>
2025-08-09 18:05:03 +0900

    [Y26W2-202] feat: 유저-여행 보드 DB 로직 구현

--- src/main/java/com/yapp/backend/repository/impl/UserTripBoardRepositoryImpl.java
+++ src/main/java/com/yapp/backend/repository/impl/UserTripBoardRepositoryImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * 사용자-여행보드 매핑 Repository 구현체
@@ -57,5 +56,11 @@ public class UserTripBoardRepositoryImpl implements UserTripBoardRepository {
         UserTripBoardEntity entity = userTripBoardMapper.domainToEntity(userTripBoard);
         UserTripBoardEntity savedEntity = jpaUserTripBoardRepository.save(entity);
         return userTripBoardMapper.entityToDomain(savedEntity);
+    }
+
+    @Override
+    public Optional<UserTripBoard> findByInvitationUrl(String invitationUrl) {
+        return jpaUserTripBoardRepository.findByInvitationUrlAndInvitationActiveTrue(invitationUrl)
+                .map(userTripBoardMapper::entityToDomain);
     }
 }
