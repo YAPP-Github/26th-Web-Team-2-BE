a9d89d51e9ee02cce383f1498f7c856c66867c80
Sungyeon <csy357@gmail.com>
2025-08-12 01:12:12 +0900

    [Y26W2-213] fix: API method를 patch로 변경

--- src/main/java/com/yapp/backend/controller/TripBoardController.java
+++ src/main/java/com/yapp/backend/controller/TripBoardController.java
@@ -32,6 +32,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -190,7 +191,7 @@ public class TripBoardController implements TripBoardDocs {
      */
     @Override
     @RequirePermission(value = PermissionType.TRIP_BOARD_MODIFY, paramName = "tripBoardId")
-    @PutMapping("/{tripBoardId}/invitation/toggle")
+    @PatchMapping("/{tripBoardId}/invitation/toggle")
     public ResponseEntity<StandardResponse<InvitationToggleResponse>> toggleInvitationActive(
             @PathVariable Long tripBoardId,
             @AuthenticationPrincipal CustomUserDetails userDetails) {
