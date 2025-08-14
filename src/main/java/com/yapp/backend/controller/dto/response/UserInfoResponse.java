52d032b16917838ab2f9947dddbfb04207f4d296
Sungyeon <csy357@gmail.com>
2025-08-14 22:03:58 +0900

    [Y26W2-298] feat: 유저 프로필 정보 조회 API

---
+++ src/main/java/com/yapp/backend/controller/dto/response/UserInfoResponse.java
@@ -0,0 +1,17 @@
+package com.yapp.backend.controller.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Schema(description = "유저 정보 응답")
+public class UserInfoResponse {
+    
+    @Schema(description = "유저 닉네임", example = "홍길동")
+    private String nickname;
+    
+    @Schema(description = "유저 프로필 이미지 URL", example = "https://example.com/profile/user123.jpg")
+    private String profileImageUrl;
+}
