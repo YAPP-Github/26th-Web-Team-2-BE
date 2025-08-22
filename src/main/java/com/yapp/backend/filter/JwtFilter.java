f17ed56ab2812b800a1a58b5969a13d3725d0e6a
Sungyeon <csy357@gmail.com>
2025-08-23 02:47:30 +0900

    [Y26W2-373] fix: 코드래빗 리뷰 반영

--- src/main/java/com/yapp/backend/filter/JwtFilter.java
+++ src/main/java/com/yapp/backend/filter/JwtFilter.java
@@ -65,6 +65,7 @@ public class JwtFilter extends OncePerRequestFilter {
                 || uri.startsWith("/swagger-ui")
                 || uri.startsWith("/v3/api-docs")
                 || uri.startsWith("/actuator/metrics")
+                || uri.startsWith("/actuator/prometheus")
                 ;
     }
 
